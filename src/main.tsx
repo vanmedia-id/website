d1ec246aaac95d2f4ddae3cfbf4bb53087143154
Ramadhan Kalih <ramadhan.kalihsewu1@gmail.com>
2026-08-25 10:38:14 +0700

    feat: add vercel speed-insights module

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,12 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 import { Toaster } from '@/components/ui/sonner'
 import { router } from '@/router'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
     <Toaster position="top-center" />
+    <SpeedInsights />
   </StrictMode>,
 )
