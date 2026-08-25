b29532a47bc85f9eb5fb727b2550a27a3a315579
Ramadhan Kalih <ramadhan.kalihsewu1@gmail.com>
2026-08-25 10:45:04 +0700

    feat: add vercel analytics module

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,17 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 import { Toaster } from '@/components/ui/sonner'
 import { router } from '@/router'
+
+import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/* Application */}
     <RouterProvider router={router} />
     <Toaster position="top-center" />
+    {/* Deployment Services */}
+    <Analytics />
     <SpeedInsights />
   </StrictMode>,
 )
